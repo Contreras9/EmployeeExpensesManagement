d7e8c012f4701b3bba083a4294e1b04eef57fa8c
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-14 21:42:38 -0600

    Working with classes-Getter and setter methods

--- src/Employee.java
+++ src/Employee.java
@@ -6,4 +6,56 @@ public class Employee {
     private String surname;
     private String jobTitle;
     private String department;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 2) {
+            System.out.println("Error - First name must be at least 2 characters.");
+        }
+        else {
+            this.firstName = firstName;
+        }
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
