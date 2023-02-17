e06ea647f0fcccf2347818c2002d5d269819b7c0
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-17 00:36:51 -0600

    Arrays Of Objects-Creating wrapper classes

---
+++ src/Employees.java
@@ -0,0 +1,41 @@
+public class Employees {
+
+    private Employee[] employees;
+
+    public Employees(int numberOfEmployees) {
+        employees = new Employee[numberOfEmployees];
+    }
+
+    public void addEmployee(Employee employee) {
+        int firstPositionEmpty = -1;
+        for (int i = 0; i < employees.length; i++) {
+            if (firstPositionEmpty == -1 && employees[i] == null) {
+                firstPositionEmpty = i;
+            }
+        }
+        if (firstPositionEmpty == -1) {
+            System.out.println("Sorry - The array is full!");
+        }
+        else {
+            employees[firstPositionEmpty] = employee;
+        }
+    }
+
+    public void printEmployees() {
+        for (Employee e : employees) {
+            if (e != null) {
+                System.out.println(e.getMailingName());
+            }
+        }
+    }
+
+    public Employee findBySurname(String surname) {
+        for (Employee e : employees) {
+            if (e != null && e.getSurname().equals(surname)) {
+                System.out.println(surname);
+                return e;
+            }
+        }
+        return null;
+    }
+}
