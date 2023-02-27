afd92207e132f564583b2e292ab4d79b328c6f53
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-26 18:08:15 -0600

    Console I/O-Using the scanner

---
+++ src/com/virtualpairprogrammers/expenses/RegisterNewEmployee.java
@@ -0,0 +1,32 @@
+package com.virtualpairprogrammers.expenses;
+
+import com.virtualpairprogrammers.expenses.domain.Department;
+import com.virtualpairprogrammers.expenses.domain.Employee;
+
+import java.util.Scanner;
+
+public class RegisterNewEmployee {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Employee employee = new Employee();
+        System.out.println("Enter the title:");
+        String title = scanner.nextLine();
+        employee.setTitle(title);
+
+        System.out.println("Enter the first name:");
+        String firstName = scanner.nextLine();
+        employee.setFirstName(firstName);
+
+        System.out.println("Enter the surname:");
+        String surname = scanner.nextLine();
+        employee.setSurname(surname);
+
+        System.out.println("Enter the department:");
+        String department = scanner.nextLine();
+        Department deptAssigned = Department.valueOf(department.toUpperCase());
+        employee.setDepartment(deptAssigned);
+
+
+        System.out.println(employee);
+    }
+}
